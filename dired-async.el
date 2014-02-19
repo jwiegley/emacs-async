3a23aa10628334e1820bc3cfbd18253e0e368f30
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-02-19 16:45:31 +0100

    * dired-async.el (helm-async-be-async): Move it as obsolete variable alias to helm.

--- dired-async.el
+++ dired-async.el
@@ -75,8 +75,6 @@ This allow to turn off async features provided to this package."
   :group 'dired-async
   :type  'boolean)
 
-(defvaralias 'helm-async-be-async 'dired-async-be-async)
-
 (defface dired-async-message
     '((t (:foreground "yellow")))
   "Face used for mode-line message."
