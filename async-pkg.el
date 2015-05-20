12235bd0e68a46fdbaade91c8d43621e8c5b293e
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-05-20 09:45:00 +0200

    * async-pkg.el: Update version number.

--- async-pkg.el
+++ async-pkg.el
@@ -1,6 +1,6 @@
 ;;; async-pkg.el
 
-(define-package "async" "1.2" "Asynchronous processing in Emacs" 'nil)
+(define-package "async" "1.3" "Asynchronous processing in Emacs" 'nil)
 
 ;; Local Variables:
 ;; no-byte-compile: t
