e8db6cc6f717f5ebd92e17abb1c7931324b29fd8
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-05-13 11:27:24 +0300

    Update pkg file.

--- async-pkg.el
+++ async-pkg.el
@@ -1,2 +1,2 @@
 ;; Generated package description from async.el
-(define-package "async" "1.6" "Asynchronous processing in Emacs" 'nil :url "http://elpa.gnu.org/packages/async.html" :keywords '("async"))
+(define-package "async" "1.9" "Asynchronous processing in Emacs" 'nil :url "http://elpa.gnu.org/packages/async.html" :keywords '("async"))
