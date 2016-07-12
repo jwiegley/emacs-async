8bc0678ebca97e7b1c6e78e75e03f8dfd502d63a
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-07-12 07:35:47 +0200

    Fix home url in *pkg.el.

--- async-pkg.el
+++ async-pkg.el
@@ -1,2 +1,10 @@
-;; Generated package description from async.el
-(define-package "async" "1.9" "Asynchronous processing in Emacs" 'nil :url "http://elpa.gnu.org/packages/async.html" :keywords '("async"))
+;;; async-pkg.el --- Generated package description from async.el
+(define-package "async" "1.9"
+  "Asynchronous processing in Emacs"
+  'nil
+  :url "https://github.com/jwiegley/emacs-async"
+  :keywords '("async"))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
