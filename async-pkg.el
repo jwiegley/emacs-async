e81c685741b63d5e0612317c100636cb384e1db5
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2019-12-20 10:31:17 +0100

    Update *pkg.el file

--- async-pkg.el
+++ async-pkg.el
@@ -1,5 +1,5 @@
 ;;; async-pkg.el --- Generated package description from async.el
-(define-package "async" "1.9.3"
+(define-package "async" "1.9.4"
   "Asynchronous processing in Emacs"
   'nil
   :url "https://github.com/jwiegley/emacs-async"
