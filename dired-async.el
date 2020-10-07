4e764e410792e6dc805282d2397cabaf12e511c2
Tulio Magno Quites Machado Filho <tuliom@ascii.art.br>
2020-10-06 21:13:38 -0300

    Remove old FSF address
    The FSF moved a while ago to a new address that is available at
    https://www.fsf.org/about/contact/.
    
    Replace the old physical address with a text pointing to the GNU website
    as recommended at [1].
    
    Notice the address used in Makefile was up-to-date, but this patch is
    also modifying it in order to keep all the files identical.
    
    [1] https://www.gnu.org/licenses/gpl-howto.html

--- dired-async.el
+++ dired-async.el
@@ -19,9 +19,8 @@
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; along with GNU Emacs; see the file COPYING.  If not, see
+;; <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
