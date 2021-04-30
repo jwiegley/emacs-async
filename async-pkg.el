94850fd5ab3bd48d5e79945f2b6f3fb001234bfc
Thierry Volpiatto <thievol@posteo.net>
2021-04-30 19:25:18 +0200

    Reinstall async-pkg.el after elpa merge

---
+++ async-pkg.el
@@ -0,0 +1,10 @@
+;;; async-pkg.el --- Generated package description from async.el
+(define-package "async" "1.9.4"
+  "Asynchronous processing in Emacs"
+  '((emacs "24.3"))
+  :url "https://github.com/jwiegley/emacs-async"
+  :keywords '("async"))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
